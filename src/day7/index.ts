9af5447c9c180e7191a4ad679c36bf0c50567006
David Kane <david.kane@photobox.com>
2021-12-07 10:15:08 +0000

    Optimise

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -1,17 +1,13 @@
-const calculateMinFuel = (crabs: number[], constantRate: boolean): number => {
-  let lowestFuel = Infinity;
+const mean = (crabs: number[]) => crabs.reduce((a, b) => a + b) / crabs.length;
+const median = (crabs: number[]) => crabs.slice().sort((a, b) => a - b)[Math.floor(crabs.length / 2)];
+const calculateFuel = (crabs: number[], position: number, constantRate: boolean) =>
+  crabs.reduce((fuel, crab) => {
+    const delta = Math.abs(crab - position);
+    return fuel + (constantRate ? delta : delta * ((delta + 1) / 2));
+  }, 0);
 
-  for (let i = Math.min(...crabs); i <= Math.max(...crabs); i++) {
-    const fuel = crabs.reduce((sum, crab) => {
-      const diff = Math.abs(crab - i);
-      return sum + (constantRate ? diff : diff * ((diff + 1) / 2));
-    }, 0);
-
-    lowestFuel = Math.min(fuel, lowestFuel);
-  }
-
-  return lowestFuel;
+export const part1 = (crabs: number[]) => calculateFuel(crabs, median(crabs), true);
+export const part2 = (crabs: number[]) => {
+  const meanCrab = mean(crabs);
+  return Math.min(calculateFuel(crabs, Math.ceil(meanCrab), false), calculateFuel(crabs, Math.floor(meanCrab), false));
 };
-
-export const part1 = (crabs: number[]) => calculateMinFuel(crabs, true);
-export const part2 = (crabs: number[]) => calculateMinFuel(crabs, false);
